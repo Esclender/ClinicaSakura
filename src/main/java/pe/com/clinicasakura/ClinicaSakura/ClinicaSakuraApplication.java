5c8db9ee46f6743cf9278fc665cf221514eb25ca
Esclender <elesclenderlugo@gmail.com>
2024-05-09 22:08:50 -0500

    Feat: Base Project

---
+++ src/main/java/pe/com/clinicasakura/ClinicaSakura/ClinicaSakuraApplication.java
@@ -0,0 +1,13 @@
+package pe.com.clinicasakura.ClinicaSakura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaSakuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaSakuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/clinicasakura/ClinicaSakura/ClinicaSakuraApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.clinicasakura.ClinicaSakura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaSakuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
